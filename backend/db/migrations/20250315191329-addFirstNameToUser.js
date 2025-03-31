8cffcf20fd37477e81dea11328f26ee59172715e
Crios223 <crios2231993@gmail.com>
2025-03-31 15:56:15 -0400

    render

--- backend/db/migrations/20250315191329-addFirstNameToUser.js
+++ backend/db/migrations/20250315191329-addFirstNameToUser.js
@@ -1,3 +1,36 @@
+// "use strict";
+
+// const { sequelize } = require("../models");
+
+// let options = {};
+// if (process.env.NODE_ENV === "production") {
+//   options.schema = process.env.SCHEMA; // define your schema in options object
+// }
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addColumn(
+//       { tableName: "Users", schema: options.schema }, // Use schema if needed
+//       "firstName", // Column name
+//       {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//         defaultValue: "", // Provide a default value for existing records
+//       }
+//     );
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeColumn(
+//       { tableName: "Users", schema: options.schema }, // Use schema if needed
+//       "firstName" // Column name
+//     );
+//   },
+// };
+
+
+
 "use strict";
 
 const { sequelize } = require("../models");
@@ -10,24 +43,38 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
     await queryInterface.addColumn(
       { tableName: "Users", schema: options.schema }, // Use schema if needed
       "firstName", // Column name
       {
         type: Sequelize.STRING,
         allowNull: false,
-        defaultValue: "", // Provide a default value for existing records
       }
     );
   },
 
   async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
     await queryInterface.removeColumn(
       { tableName: "Users", schema: options.schema }, // Use schema if needed
       "firstName" // Column name
     );
   },
 };
+
 
 
 
--- backend/db/migrations/20250315200508-addLastNameToUser.js
+++ backend/db/migrations/20250315200508-addLastNameToUser.js
@@ -1,3 +1,37 @@
+// "use strict";
+
+// const { sequelize } = require("../models");
+
+// let options = {};
+// if (process.env.NODE_ENV === "production") {
+//   options.schema = process.env.SCHEMA; // define your schema in options object
+// }
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addColumn(
+//       { tableName: "Users", schema: options.schema }, // Use schema if needed
+//       "lastName", // Column name
+//       {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//         defaultValue: "", // Provide a default value for existing records
+//       }
+//     );
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeColumn(
+//       { tableName: "Users", schema: options.schema }, // Use schema if needed
+//       "lastName" // Column name
+//     );
+//   },
+// };
+
+
+
+
 "use strict";
 
 const { sequelize } = require("../models");
@@ -10,18 +44,31 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
     await queryInterface.addColumn(
       { tableName: "Users", schema: options.schema }, // Use schema if needed
       "lastName", // Column name
       {
         type: Sequelize.STRING,
         allowNull: false,
-        defaultValue: "", // Provide a default value for existing records
       }
     );
   },
 
   async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  
+
     await queryInterface.removeColumn(
       { tableName: "Users", schema: options.schema }, // Use schema if needed
       "lastName" // Column name
