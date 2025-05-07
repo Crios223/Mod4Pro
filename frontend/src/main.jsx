3d258dbeb74ba856428334e54769740b48db8b15
Crios223 <crios2231993@gmail.com>
2025-05-07 16:48:54 -0400

    fixing errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,44 +1,11 @@
-// // frontend/src/main.jsx
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css';
-// import { Provider } from 'react-redux';
-
-
-
-// import configureStore from './store/store.js';
-
-// const store = configureStore();
-
-// if (process.env.NODE_ENV !== 'production') {
-//   window.store = store;
-// }
-
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// );
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux';
-import configureStore from './store/store.js';
+import configureStore from './store';
 
 const store = configureStore();
-
-if (import.meta.env.MODE !== 'production') {
-  window.store = store;
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
